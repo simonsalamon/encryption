99e2ababc82dcac798b3604e2384b2d8deffe8c1
mohammadreza <salamon.1378@gmail.com>
2023-08-27 11:24:58 +0330

    Add project files.

---
+++ Assets/scripts/mono.cs
@@ -0,0 +1,45 @@
+﻿using Org.BouncyCastle.Crypto.Parameters;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    private byte[] key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createKey();
+
+        //var (ciphertext, nonce, tag) = encrypt.EncryptData("hi dadash " , key);
+
+
+        //Debug.Log(Encoding.UTF8.GetString(ciphertext));
+
+        //var text = encrypt.Decrypt(ciphertext, nonce, tag , key);
+
+        //Debug.Log(text);
+
+
+
+        var(ciphertext, nonce, tag) = newEncrypt.EncryptWithBouncyCastle( "سلام داداش ", key);
+        Debug.Log(Encoding.UTF8.GetString(ciphertext));
+
+
+        string encrypedData = newEncrypt.DecryptWithBouncyCastle(ciphertext , nonce , tag , key);
+
+        Debug.Log(encrypedData);
+
+    }
+
+    public void createKey()
+    {
+
+        key = new byte[32];
+        RandomNumberGenerator.Fill(key);
+    }
+}
---
+++ Assets/scripts/newEncrypt.cs
@@ -0,0 +1,54 @@
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto;
+using System;
+using System.Text;
+using System.Linq;
+using System.Security.Cryptography;
+
+public class newEncrypt
+{
+    public static (byte[] ciphertext, byte[] nonce, byte[] tag) EncryptWithBouncyCastle(string plaintext, byte[] key)
+    {
+        const int nonceLength = 12; // in bytes
+        const int tagLength = 16; // in bytes
+
+        var nonce = new byte[nonceLength];
+        RandomNumberGenerator.Fill(nonce);
+
+        var plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+        var bouncyCastleCiphertext = new byte[plaintextBytes.Length + tagLength];
+
+        var cipher = new GcmBlockCipher(new AesEngine());
+        var parameters = new AeadParameters(new KeyParameter(key), tagLength * 8, nonce);
+        cipher.Init(true, parameters);
+
+        var offset = cipher.ProcessBytes(plaintextBytes, 0, plaintextBytes.Length, bouncyCastleCiphertext, 0);
+        cipher.DoFinal(bouncyCastleCiphertext, offset);
+
+        // Bouncy Castle includes the authentication tag in the ciphertext
+        var ciphertext = new byte[plaintextBytes.Length];
+        var tag = new byte[tagLength];
+        Buffer.BlockCopy(bouncyCastleCiphertext, 0, ciphertext, 0, plaintextBytes.Length);
+        Buffer.BlockCopy(bouncyCastleCiphertext, plaintextBytes.Length, tag, 0, tagLength);
+
+        return (ciphertext, nonce, tag);
+    }
+
+    public static string DecryptWithBouncyCastle(byte[] ciphertext, byte[] nonce, byte[] tag, byte[] key)
+    {
+        var plaintextBytes = new byte[ciphertext.Length];
+
+        var cipher = new GcmBlockCipher(new AesEngine());
+        var parameters = new AeadParameters(new KeyParameter(key), tag.Length * 8, nonce);
+        cipher.Init(false, parameters);
+
+        var bouncyCastleCiphertext = ciphertext.Concat(tag).ToArray();
+
+        var offset = cipher.ProcessBytes(bouncyCastleCiphertext, 0, bouncyCastleCiphertext.Length, plaintextBytes, 0);
+        cipher.DoFinal(plaintextBytes, offset);
+
+        return Encoding.UTF8.GetString(plaintextBytes);
+    }
+}
